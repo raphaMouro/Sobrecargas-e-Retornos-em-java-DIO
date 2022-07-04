08026becc7997714e0d266cbe9baee8dbf3b237f
raphaMouro <raphael.phgru@gmail.com>
2022-07-04 11:45:21 -0300

    first commit

---
+++ src/br/com/dio/Quadrilatero.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+public class Quadrilatero {
+
+    public static double area(double lado) {
+        return lado * lado;
+    }
+
+    public static double area(double lado1, double lado2) {
+        return lado1 * lado2;
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura){
+        return ((baseMenor+baseMenor)*altura) / 2;
+
+    }
+
+}
